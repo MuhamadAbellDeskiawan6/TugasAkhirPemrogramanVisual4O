d00d6a28d17261c1e91138a503ca6825cd0fc282
muhamadabelldeskiawan <muhamadabelldeskiawan@gmail.com>
2023-06-21 18:44:02 +0800

    commit ketiga membuat form poin

--- Project1.dpr
+++ Project1.dpr
@@ -4,12 +4,14 @@ uses
   Forms,
   Unit1 in 'unit1\Unit1.pas' {Form1},
   Unit2 in 'unit2\Unit2.pas' {Form2},
-  Unit3 in 'unit3\Unit3.pas' {Form3};
+  Unit3 in 'unit3\Unit3.pas' {Form3},
+  Unit4 in 'unit4\Unit4.pas' {Form4};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TForm4, Form4);
   Application.CreateForm(TForm3, Form3);
   Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TForm1, Form1);
