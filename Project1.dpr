32823a8cc30f91854237786cf266e3c8dc553dee
muhamadabelldeskiawan <muhamadabelldeskiawan@gmail.com>
2023-06-22 18:51:43 +0800

    commit keempat membuat form ortu

--- Project1.dpr
+++ Project1.dpr
@@ -5,12 +5,14 @@ uses
   Unit1 in 'unit1\Unit1.pas' {Form1},
   Unit2 in 'unit2\Unit2.pas' {Form2},
   Unit3 in 'unit3\Unit3.pas' {Form3},
-  Unit4 in 'unit4\Unit4.pas' {Form4};
+  Unit4 in 'unit4\Unit4.pas' {Form4},
+  Unit5 in 'unit5\Unit5.pas' {Form5};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TForm5, Form5);
   Application.CreateForm(TForm4, Form4);
   Application.CreateForm(TForm3, Form3);
   Application.CreateForm(TForm2, Form2);
