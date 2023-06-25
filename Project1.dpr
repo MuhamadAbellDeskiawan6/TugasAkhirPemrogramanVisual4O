9ae9812757456fd81dc5d5b3f60255ece739a7d2
muhamadabelldeskiawan <muhamadabelldeskiawan@gmail.com>
2023-06-25 20:35:57 +0800

    commit ketujuh membuat form user dan memperbaiki form siswa

--- Project1.dpr
+++ Project1.dpr
@@ -8,18 +8,20 @@ uses
   Unit4 in 'unit4\Unit4.pas' {Form4},
   Unit5 in 'unit5\Unit5.pas' {Form5},
   Unit6 in 'unit6\Unit6.pas' {Form6},
-  Unit7 in 'unit7\Unit7.pas' {Form7};
+  Unit7 in 'unit7\Unit7.pas' {Form7},
+  Unit8 in 'unit8\Unit8.pas' {Form8};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TForm8, Form8);
+  Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TForm7, Form7);
   Application.CreateForm(TForm6, Form6);
   Application.CreateForm(TForm5, Form5);
   Application.CreateForm(TForm4, Form4);
   Application.CreateForm(TForm3, Form3);
   Application.CreateForm(TForm2, Form2);
-  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
