9153fb8c824b082a4afa1931caf75edb0184bea8
muhamadabelldeskiawan <muhamadabelldeskiawan@gmail.com>
2023-06-25 16:22:08 +0800

    commit keenam membuat form semester

--- Project1.dpr
+++ Project1.dpr
@@ -7,12 +7,14 @@ uses
   Unit3 in 'unit3\Unit3.pas' {Form3},
   Unit4 in 'unit4\Unit4.pas' {Form4},
   Unit5 in 'unit5\Unit5.pas' {Form5},
-  Unit6 in 'unit6\Unit6.pas' {Form6};
+  Unit6 in 'unit6\Unit6.pas' {Form6},
+  Unit7 in 'unit7\Unit7.pas' {Form7};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TForm7, Form7);
   Application.CreateForm(TForm6, Form6);
   Application.CreateForm(TForm5, Form5);
   Application.CreateForm(TForm4, Form4);
